3296d409d6b5e30f172861231daacae5edcb67cc
Asha Fernandez <ash.fernan90@gmail.com>
2020-01-23 01:27:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -19,18 +19,22 @@ class Owner
 
   def self.count
     self.all.count
+    
   end
 
   def self.reset_all
     self.all.clear
+    
   end
 
   def cats
     Cat.all.select{|cat| cat.owner == self}
+    
   end
 
   def dogs
     Dog.all.select{|dog| dog.owner == self}
+    
   end
 
   def buy_cat(name)
@@ -43,10 +47,12 @@ class Owner
 
   def walk_dogs
     self.dogs.each { |dog| dog.mood = "happy" }
+    
   end
 
   def feed_cats
     self.cats.each { |cat| cat.mood = "happy" }
+    
   end
 
   def sell_pets
@@ -55,10 +61,13 @@ class Owner
     pets.each do |pet|
       pet.mood = "nervous"
       pet.owner = nil
+      
     end
+    
   end
 
   def list_pets
     "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
+  
 end
